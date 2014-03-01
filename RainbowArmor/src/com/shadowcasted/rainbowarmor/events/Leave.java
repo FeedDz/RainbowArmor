2ecdc65d51bf122196122168396bf83573af868d
Jona123 <jcdra1@gmail.com>
2014-03-01 18:04:21 +0100

    edit three (Fixed everything up!)

---
+++ RainbowArmor/src/com/shadowcasted/rainbowarmor/events/Leave.java
@@ -0,0 +1,20 @@
+package com.shadowcasted.rainbowarmor.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import com.shadowcasted.rainbowarmor.runnables.RainbowRunnable1;
+
+public class Leave implements Listener{
+@EventHandler
+public void quit(PlayerQuitEvent e){
+	for (Player p:Bukkit.getOnlinePlayers()){
+		if (RainbowRunnable1.players.contains(p.getName())){
+			com.shadowcasted.rainbowarmor.resources1.Main.revertArmor(p);
+		}
+	}
+}
+}
